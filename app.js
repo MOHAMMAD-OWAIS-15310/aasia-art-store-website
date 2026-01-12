c2e20550db34386c32eda3a83df6c550e690b3a7
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2026-01-13 00:00:22 +0530

    initialize db and crud operations

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const mongoose = require("mongoose");
 const app=express();
+const Listing = require("./models/listing.js");
+const path=require("path");
+const methodOverride=require("method-override");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/test";
+const MONGO_URL="mongodb://127.0.0.1:27017/artStore";
 
 main()
     .then(()=>{
@@ -15,10 +18,71 @@ main()
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("working");
 });
+
+//index
+app.get("/listings",async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index",{allListings});
+});
+
+//new
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//show rroute
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listing= await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+})
+
+//create
+app.post("/listings",async(req,res)=>{
+    // let listing =req.body.listing;
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    // console.log(newListing);
+    res.redirect("/listings");
+})
+
+//edit
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//update
+app.put("/listings/:id" , async(req,res) =>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+})
+
+
+// app.get("/testlisting", async (req,res)=>{
+//     let sampleListing= new Listing({
+//         title: "trees",
+//         price: 100,
+//         size : "medium" , 
+//         medium :"guache" ,
+//         available :"yes" ,
+//         image :"C:\Users\Ahmed Taqiuddin\Pictures\Aasia's collection (p)\IMG_20220615_150541_197.jpg" ,
+//     });
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful");
+// })
+
 app.listen(3000,()=>{
     console.log("app is listening to port 3000");
 });
---
+++ init/data.js
@@ -0,0 +1,28 @@
+const sampleListing = [
+    {
+        title: "sky",
+        price: 100,
+        size : "medium" , 
+        medium :"guache" ,
+        available :"yes" ,
+        image :"https://plus.unsplash.com/premium_photo-1673643405538-de0f82933fcb?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ,
+    },
+    {
+        title: "mountain",
+        price: 100,
+        size : "medium" , 
+        medium :"guache" ,
+        available :"yes" ,
+        image :"https://plus.unsplash.com/premium_photo-1673643405538-de0f82933fcb?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ,
+    },
+    {
+        title: "river",
+        price: 100,
+        size : "medium" , 
+        medium :"guache" ,
+        available :"no" ,
+        image :"https://plus.unsplash.com/premium_photo-1673643405538-de0f82933fcb?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ,
+    },
+]
+
+module.exports = {data : sampleListing };
