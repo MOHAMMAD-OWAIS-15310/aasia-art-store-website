91b9cfc3b030b8f14b8258a55855d69d2cee4407
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2026-07-11 00:39:40 +0530

    working on order feature : order schema

---
+++ models/order.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const  orderSchema =new Schema(
+        {
+        user:{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+          paintings:[
+            {
+                type : Schema.Types.ObjectId,
+                ref: "Listing",
+
+                required: true,
+            },
+        ],
+
+
+
+         name:{
+            type:String, 
+            
+            required: true,
+        },
+        phone:  {
+            type:String,
+            required: true,
+        },
+
+        address :{
+            type: String,
+            required:true,
+        },
+
+            city:{
+                type : String,
+                required: true,
+            },
+
+        state:{
+            type: String,
+            required: true,
+        },
+
+        pincode: {
+            type : String,
+        required: true,
+        },
+
+        totalAmount :  {
+            type :Number,
+            required :true,
+        },
+
+        paymentMethod :{
+            type: String,
+            enum: ["COD"],
+            default: "COD",
+        },
+
+        status: {
+            type: String,
+            enum:[
+                " Pending ",
+                "Confirmed ",
+                " Packed",
+                "Shipped ",
+                " Delivered",
+                "Cancelled",
+            ],
+            default: "Pending",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
